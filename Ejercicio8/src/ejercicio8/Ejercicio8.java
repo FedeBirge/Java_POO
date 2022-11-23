e927b1cfd7828b09a4f3170083e03bab12616949
FedeBirge <fedeikky@gmail.com>
2022-11-22 21:57:28 -0300

    actualizo ejercicio 8

---
+++ Ejercicio8/src/ejercicio8/Ejercicio8.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio8;
+
+import Entidad.Cadena;
+import Servicio.CadenaService;
+import java.util.Scanner;
+
+/**
+ *
+ * @author feder
+ */
+public class Ejercicio8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        CadenaService cadServ = new CadenaService();
+        Scanner scan = new Scanner(System.in).useDelimiter("\n");
+        Cadena cad = cadServ.crearCadena();
+        System.out.println("La cantidad de vocales es "+cadServ.mostrarVocales(cad));
+        cadServ.invertirFrase(cad);
+        System.out.println(cad.getFrase());
+        System.out.println("Ingrese una caracter: ");
+        String car= scan.next();
+        System.out.println("el caracter se repite: "+cadServ.vecesRepetido(cad, car)+" veces");
+        System.out.println("Ingrese una frase: ");
+        if (cadServ.compararLongitud(cad, scan.next())){
+            System.out.println("Tienen la misma longitud ");
+        }
+        else{
+            System.out.println("NO tienen la misma longitud");
+            }
+    }
+    
+}
