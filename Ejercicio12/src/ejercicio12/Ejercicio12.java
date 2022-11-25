068dd2b1740c6a15f85801615acf84378cb6604e
FedeBirge <fedeikky@gmail.com>
2022-11-24 22:03:05 -0300

    actualizo ejercicio 12

---
+++ Ejercicio12/src/ejercicio12/Ejercicio12.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio12;
+
+import Entidad.Persona;
+import Servicio.PersonaService;
+
+/**
+ *
+ * @author feder
+ */
+public class Ejercicio12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Persona per = new Persona();
+       PersonaService serv = new PersonaService();
+       serv.crearPersona(per);
+        System.out.println("");
+        System.out.println("LA edad es:"+serv.calcularEdad(per));
+       
+    }
+    
+}
