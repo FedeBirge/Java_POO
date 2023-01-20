ae0157a0d88eda00f6256290f35fd34191e56636
FedeBirge <fedeikky@gmail.com>
2023-01-19 21:16:14 -0300

    actualizo  colecciones ejer 6

--- Colecciones/src/Service/ServiceProducto.java
+++ Colecciones/src/Service/ServiceProducto.java
@@ -64,13 +64,9 @@ public class ServiceProducto {
         if (productos.containsKey(nombre)) {
             System.out.println("Ingrese el precio actualizado");
             Integer valor = scan.nextInt();
+            productos.put(nombre,valor);    
 
-            for (Map.Entry<String, Integer> prod : productos.entrySet()) {
-                String key = prod.getKey();
-                if (key.equals(nombre)) {
-                    prod.setValue(valor);
-                }
-            }
+           
         } else {
             System.out.println("El producto no existe");
         }
